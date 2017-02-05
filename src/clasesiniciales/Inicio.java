94b9b456811e120a8cd0e7e2ef8f03453190a4e5
Albin Nunez <albin.ernesto78@hotmail.com>
2017-02-05 16:22:59 -0500

    Proyecto_De_Prueba

---
+++ src/clasesiniciales/Inicio.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesiniciales;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author SP
+ */
+public class Inicio extends Inicio_Parte_Dos implements Serializable {
+
+    private int cont;
+    public int cont2;
+    protected int cont3;   
+    int cont4;
+
+   // Inicio_Parte_Dos Objeto1; */  
+    public int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public void mostrar() {
+
+    }
+
+}
